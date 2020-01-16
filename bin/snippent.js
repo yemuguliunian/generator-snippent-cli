f2124a70ded6067d1415e61b7e9c1cb155c5c1c1
yemu <215822834@qq.com>
2020-01-16 15:49:56 +0800

    refactor: 代码块使用字符串模板

--- bin/snippent.js
+++ bin/snippent.js
@@ -6,10 +6,6 @@ const run = require('../lib/run');
 
 program
     .version(package.version)
-    .option('-s, --sublime', 'generate snippent of sublime ')
-    .option('-v, --vscode', 'generate snippent of vscode')
     .parse(process.argv);
 
-if (!program.sublime && !program.vscode) {
-    run();
-}
+run();
